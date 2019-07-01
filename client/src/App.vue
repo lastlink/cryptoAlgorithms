b01e3fe6aeb93ce0b5f802c17a2114361e027373
lastlink <theefunk@gmail.com>
2019-07-01 18:48:30 -0400

    working request

--- client/src/App.vue
+++ client/src/App.vue
@@ -14,7 +14,7 @@
       </select>
       <br />
       <label>algorithm</label>
-      <select v-model="comparisonOne.selectedEnv">
+      <select v-model="comparisonOne.algorithm">
         <option disabled value>Please select one</option>
         <option>bcrypt</option>
         <option>sha256</option>
@@ -29,7 +29,7 @@
 
       <br />
       <label>password</label>
-      <input type="text" v-model="password" />
+      <input type="text" v-model="comparisonOne.password" />
 
       <br />
       <label v-if="comparisonOne.hash_verify=='verify'">hashed password</label>
@@ -40,24 +40,25 @@
       />
 
       <br />
-      <label v-if="comparisonOne.selectedEnv && comparisonOne.selectedEnv!='bcrypt'">iterations</label>
+      <label v-if="comparisonOne.algorithm && comparisonOne.algorithm!='bcrypt'">iterations</label>
       <input
         type="number"
-        v-if="comparisonOne.selectedEnv && comparisonOne.selectedEnv!='bcrypt'"
+        v-if="comparisonOne.algorithm && comparisonOne.algorithm!='bcrypt'"
         v-model="comparisonOne.iterations"
       />
 
       <br />
-      <label v-if="comparisonOne.selectedEnv && comparisonOne.selectedEnv!='bcrypt'">salt</label>
+      <label v-if="comparisonOne.algorithm && comparisonOne.algorithm!='bcrypt'">salt</label>
       <input
         type="text"
-        v-if="comparisonOne.selectedEnv && comparisonOne.selectedEnv!='bcrypt'"
+        v-if="comparisonOne.algorithm && comparisonOne.algorithm!='bcrypt'"
         v-model="comparisonOne.salt"
       />
 
       <br />
       <button v-on:click="sendRequest(1)">Send Request</button>
-      <textarea readonly v-model="comparisonOne.response"></textarea>
+      <!-- <textarea readonly v-model="comparisonOne.response"></textarea> -->
+       <pre v-if="comparisonOne.response">{{ comparisonOne.response | pretty }}</pre>
     </div>
   </div>
 </template>
@@ -91,7 +92,43 @@ export default {
       if (compareNum == 1) temp = this.comparisonOne;
       else temp = this.comparisonTwo;
 
+      // var request
       console.log(temp);
+      return this.$http
+        .post(temp.selectedEnv+`/hashes`, temp, {
+          cache: "no-cache",
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+        .then(response => {
+          this.loading = false;
+          console.log(response)
+          // this.message = response.data.message;
+          if (response.status === 200) {
+            // console.log(JSON.parse(JSON.stringify(response.data)))
+            var resources = response.body;
+
+          
+            this.comparisonOne.response = resources;
+            // this.comparisonOne = JSON.parse(JSON.stringify(this.comparisonOne))
+            console.log(this.comparisonOne.response)
+          } else {
+            this.errors = "Failed to Load";
+          }
+        })
+        .catch(error => {
+          this.errors = "Failed to Retrieve issues";
+          console.log(error);
+          this.loading = null;
+        });
+    },
+  },
+  filters: {
+    pretty: function(value) {
+      // console.log(value)
+      // return value;
+      return JSON.stringify(value);
     }
   }
 };
